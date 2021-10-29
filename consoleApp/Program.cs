a30eae93cd20d972ea466f2b8ba685ec8990ff1f
Aaron Vegu <aaron@reckoner-rde.io>
2021-10-29 14:00:48 -0500

    First commit

---
+++ consoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dewey";
+            int age = 22;
+
+            Console.WriteLine("There is a man called " + name + ", his age is "
+                + age);
+
+            Console.ReadLine();
+        }
+    }
+}
